1935e57f2eb2bc392aafdfb5db8ecc21ec25b585
hyx <huyanxinag@outlook.com>
2019-09-27 16:56:55 +0800

    revertback

--- RdbMicroservice/Service/EventHostedService.cs
+++ RdbMicroservice/Service/EventHostedService.cs
@@ -22,7 +22,6 @@ namespace rdbMicroservice.Service
         private EssDbContext _context;
         private IProducer<Null, string> _producer;
         private long tag = -1;
-        private string tag2 = string.Empty;
         public IServiceProvider Services { get; }
         public EventHostedService(IServiceProvider services, IProduicerFactoryService produicerFactoryService
             //ILogger<EventHostedService> logger
@@ -56,21 +55,18 @@ namespace rdbMicroservice.Service
                             _context = scope.ServiceProvider.GetRequiredService<EssDbContext>();
                             try
                             {
-                                if (string.IsNullOrEmpty(tag2))
+                                if (tag == -1)
                                 {
-                                    //tag = _context.Events.LongCount();
-                                    tag2 = _context.Events.FromSql(" select ETime from e_log order by ETime desc limit 1 ").AsNoTracking().FirstOrDefault().ETime.ToString();
+                                    tag = _context.Events.LongCount();
                                 }
                                 else
                                 {
-                                    //var current = _context.Events.LongCount();
                                     var current = _context.Events.LongCount();
                                     var num = _context.Events.LongCount() - tag;
                                     if (num == 0)
                                         continue;
                                     //var events = _context.Events.FromSql("SELECT * FROM e_log LIMIT {0},{1}", tag, num).AsNoTracking().ToList();
                                     var events = _context.Events.FromSql(" SELECT a.ETime ,a.ETime_MS ,a.PID ,a.ELevel ,a.Ack ,a.OriginTime ,a.OriginTime_MS ,a.RestoreTime ,a.RestoreTime_MS ,a.AckTime ,a.AckTime_MS ,a.NodeID ,a.User ,a.Src ,a.Type ,a.EQDes ,a.PIDDes ,a.ValueDisplay ,a.Des ,a.StnNo ,a.StnName ,a.SpecialtyNo ,a.EQType ,a.PushGraph from e_log as a inner join ( SELECT ETime, ETime_MS, PID from e_log ORDER BY ETime limit {0},{1} ) as b on a.ETime = b.ETime and a.ETime_MS = b.ETime_MS and a.PID = b.PID ", tag, num).AsNoTracking().ToList();
-                                    //tag = current;
                                     tag = current;
                                     foreach (var e in events)
                                     {
